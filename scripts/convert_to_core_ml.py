c7fddf90a924d9630167a8d0251334f497302c87
Nick Bourdakos <bourdakos1@gmail.com>
2019-02-10 12:34:40 -0500

    break down conversion script

---
+++ scripts/convert_to_core_ml.py
@@ -0,0 +1,40 @@
+def convert_to_core_ml():
+    import tfcoreml
+
+    path = 'model_ios'
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+    # image_input_names = '{}:0'.format(args.input_name)
+    # output_feature_names = ['{}:0'.format(name) for name in args.output_names]
+    if LOCALIZATION:
+        print('core ml coming soon for object detection')
+        return
+
+        # frozen_model_file = '.tmp/tmp_frozen_graph.pb'
+        # with tf.Session(graph=tf.Graph()) as sess:
+        #     tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], args.tf_model_path) 
+        #     input_node_names = ['Preprocessor/sub']
+        #     output_node_names = ['concat', 'concat_1']
+        #     gdef = strip_unused_lib.strip_unused(
+        #             input_graph_def=sess.graph_def,
+        #             input_node_names=input_node_names,
+        #             output_node_names=output_node_names,
+        #             placeholder_type_enum=dtypes.float32.as_datatype_enum)
+        #     with gfile.GFile(frozen_model_file, "wb") as f:
+        #         f.write(gdef.SerializeToString())
+
+        # input_tensor_shapes = {'Preprocessor/sub:0':[1,300,300,3]} # batch size is 1
+        # output_tensor_names = ['concat:0', 'concat_1:0']
+
+        # tfcoreml.convert(tf_model_path=frozen_model_file,
+        #                 mlmodel_path=args.mlmodel_path,
+        #                 output_feature_names=output_tensor_names,
+        #                 # class_labels=args.class_labels,
+        #                 red_bias=-1,
+        #                 green_bias=-1,
+        #                 blue_bias=-1,
+        #                 image_scale=1.0/128.0,
+        #                 input_name_shape_dict=input_tensor_shapes)
+        #                 #  image_input_names=input_tensor_shapes)
---
+++ scripts/convert_to_tflite.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+from tensorflow.python.tools import strip_unused_lib
+from tensorflow.python.framework import dtypes
+from tensorflow.python.platform import gfile
+
+# TensorFlow 1.9 to TensorFlow 1.11
+if tf.__version__ <= '1.11.0':
+    from tensorflow.contrib.lite.python.lite import TocoConverter as convert
+elif tf.__version__ <= '1.12.0':
+    convert = tf.contrib.lite.TFLiteConverter
+else:
+    convert = tf.lite.TFLiteConverter
+
+def convert_to_tflite():
+    if LOCALIZATION:
+        print('tflite coming soon for object detection')
+        return
+
+
+    path = 'model_android'
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+    input_arrays = [args.input_name]
+    output_arrays = args.output_names
+
+    converter = convert.from_saved_model(
+            args.tf_model_path,
+            input_arrays=input_arrays,
+            # input_shapes={args.input_name : [1, 300, 300, 3]},
+            output_arrays=output_arrays)
+    tflite_model = converter.convert()
+    open(args.tflite_path, 'wb').write(tflite_model)
