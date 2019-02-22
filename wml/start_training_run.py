cc68b2d751fe3e30eaac0fd6c553f38efdd6f0ed
Nick Bourdakos <bourdakos1@gmail.com>
2019-02-21 22:41:33 -0500

    update steps flag

--- wml/start_training_run.py
+++ wml/start_training_run.py
@@ -23,7 +23,10 @@ parser.add_argument('--data-bucket-name', type=str)
 parser.add_argument('--data-bucket-endpoint', type=str)
 parser.add_argument('--gpu', type=str, default='k80')
 parser.add_argument('--num-train-steps', type=int, default=300)
+parser.add_argument('--steps', type=int, default=None)
 args = parser.parse_args()
+
+args.num_train_steps = args.steps or args.num_train_steps
 
 MODEL_ZIP_PATH = 'tf-model.zip'
 
