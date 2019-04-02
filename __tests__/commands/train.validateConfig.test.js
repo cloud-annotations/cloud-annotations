be5c58fd25f878420c0158aee47889ee33688f6e
Nick Bourdakos <bourdakos1@gmail.com>
2019-04-02 16:03:55 -0400

    write tests for validate config

--- __tests__/commands/train.validateConfig.test.js
+++ __tests__/commands/train.validateConfig.test.js
@@ -1,13 +1,10 @@
 const assert = require('assert')
-// const sinon = require('sinon')
+const sinon = require('sinon')
 const rewire = require('rewire')
 const train = rewire('./../../src/commands/train')
-// const mock = require('./../mock')
-
-const api = require('./../../src/api/api')
 
 describe('train validateConfig', () => {
-  const fakeConfig = {
+  const realCredentials = {
     name: 'bucket',
     credentials: {
       wml: {
@@ -26,24 +23,60 @@ describe('train validateConfig', () => {
     trainingParams: { gpu: 'k80', steps: '500' }
   }
 
-  it('exits with credential errors', async () => {
-    try {
-      const res = await api.authenticate()
-      console.log(res)
-    } catch {
-      console.log('bad credentials')
-    }
+  const empty = {
+    name: 'bucket',
+    credentials: {
+      wml: {
+        instance_id: '',
+        username: '',
+        password: '',
+        url: ''
+      },
+      cos: {
+        access_key_id: '',
+        secret_access_key: '',
+        region: ''
+      }
+    },
+    buckets: { training: '' }
+  }
 
-    try {
-      const res = await api.authenticate('url', 'username', 'password')
-      console.log(res)
-    } catch {
-      console.log('bad credentials')
-    }
+  const invalidRegion = {
+    name: 'bucket',
+    credentials: {
+      wml: {
+        instance_id: '',
+        username: 'username',
+        password: 'password',
+        url: 'url'
+      },
+      cos: {
+        access_key_id: 'access_key_id',
+        secret_access_key: 'secret_access_key',
+        region: 'us-geo'
+      }
+    },
+    buckets: { training: 'out-of-region', output: 'out-of-region' }
+  }
 
-    // sinon.stub(process, 'exit')
-    // train.__get__('validateConfig')(fakeConfig)
-    // assert(process.exit.called)
-    // process.exit.restore()
+  it('finishes safely with good credentials', async () => {
+    sinon.stub(process, 'exit')
+    await train.__get__('validateConfig')(realCredentials)
+    assert(!process.exit.called)
+    process.exit.restore()
+  })
+
+  it('exits with no credentials', async () => {
+    sinon.stub(process, 'exit')
+    await train.__get__('validateConfig')(empty)
+    assert(process.exit.called)
+    process.exit.restore()
+  })
+
+  it('exits with wrong region', async () => {
+    sinon.stub(process, 'exit')
+    await train.__get__('validateConfig')(invalidRegion)
+    assert(process.exit.called)
+    process.exit.restore()
   })
 })
