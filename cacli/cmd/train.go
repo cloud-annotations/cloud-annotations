6ad157e5457ce3b8840d353b74f4186ca75f22c5
Nick <bourdakos1@gmail.com>
2019-11-19 13:58:22 -0500

    add picker ui example

--- cacli/cmd/train.go
+++ cacli/cmd/train.go
@@ -22,10 +22,15 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/cloud-annotations/promptui"
 	"github.com/spf13/cobra"
 )
+
+type pepper struct {
+	Name     string
+	HeatUnit int
+	Peppers  int
+}
 
 // trainCmd represents the train command
 var trainCmd = &cobra.Command{
@@ -38,7 +43,34 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("train called")
+		peppers := []pepper{
+			{Name: "Bell Pepper", HeatUnit: 0, Peppers: 0},
+			{Name: "Banana Pepper", HeatUnit: 100, Peppers: 1},
+			{Name: "Poblano", HeatUnit: 1000, Peppers: 2},
+			{Name: "Jalapeño", HeatUnit: 3500, Peppers: 3},
+			{Name: "Aleppo", HeatUnit: 10000, Peppers: 4},
+			{Name: "Tabasco", HeatUnit: 30000, Peppers: 5},
+			{Name: "Malagueta", HeatUnit: 50000, Peppers: 6},
+		}
+
+		templates := &promptui.SelectTemplates{
+			Label:    "{{ . | bold }}s {{\"(Use arrow keys and enter to choose)\" | faint }}",
+			Active:   "{{\"❯\" | cyan | bold }} {{ .Name | cyan | bold }}",
+			Inactive: "  {{ .Name }}",
+			Selected: "{{ .Label }} {{ .Item.Name | cyan | bold }}",
+		}
+
+		prompt := promptui.Select{
+			Label:     "Pepper",
+			Items:     peppers,
+			Templates: templates,
+			Size:      11,
+			HideHelp:  true,
+		}
+
+		if _, _, err := prompt.Run(); err != nil {
+			return
+		}
 	},
 }
 
