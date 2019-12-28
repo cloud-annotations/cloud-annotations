47b19aeff69f71342cdf4455f6a8b5fa6134c07c
Nick <bourdakos1@gmail.com>
2019-12-27 19:23:34 -0500

    use python 3

--- release.sh
+++ release.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-python --version
-python3 --version
-go version
 set -e
 
 cd cacli && curl -sL https://git.io/goreleaser | bash
 
-cd ../trainer && python setup.py all
+cd ../trainer && python3 setup.py all
 curl --data-binary @training.zip -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/cloud-annotations/training/releases/$TRAVIS_TAG/assets?name=training.zip&access_token=$GITHUB_TOKEN"
