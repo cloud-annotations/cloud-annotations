08b00242de6fd2e15baaf335f66b9ab839683f84
Nick <bourdakos1@gmail.com>
2019-12-28 01:01:16 -0500

    get GitHub release id from tag

--- release.sh
+++ release.sh
@@ -9,6 +9,18 @@ rm -f protoc-3.7.1-linux-x86_64.zip
 cd cacli && curl -sL https://git.io/goreleaser | bash
 
 cd ../trainer && python setup.py all
-ls
-echo "https://uploads.github.com/repos/cloud-annotations/training/releases/$TRAVIS_TAG/assets?name=training.zip&access_token=$GITHUB_TOKEN"
-curl --data-binary @training.zip -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/cloud-annotations/training/releases/$TRAVIS_TAG/assets?name=training.zip&access_token=$GITHUB_TOKEN"
+
+# Read asset tags.
+response=$(curl -s "https://api.github.com/repos/cloud-annotations/training/releases/tags/$TRAVIS_TAG?access_token=$GITHUB_TOKEN")
+
+# Get ID of the asset based on given filename.
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || {
+  echo "Error: Failed to get release id for tag: $TRAVIS_TAG"
+  echo "$response" | awk 'length($0)<100' >&2
+  exit 1
+}
+
+# Upload asset
+echo "Uploading asset... "
+curl --data-binary @training.zip -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/cloud-annotations/training/releases/$id/assets?name=training.zip&access_token=$GITHUB_TOKEN"
