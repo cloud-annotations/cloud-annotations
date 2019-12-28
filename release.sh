1d4bbf2b9335a6f5d8eca0aaaa0ec760d4f8fe3b
Nick <bourdakos1@gmail.com>
2019-12-27 19:04:15 -0500

    use a release script

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+echo $TRAVIS_TAG
+cd cacli && curl -sL https://git.io/goreleaser | bash
+
+cd ../trainer && python setup.py all
+curl --data-binary @training.zip -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/cloud-annotations/training/releases/$TRAVIS_TAG/assets?name=training.zip&access_token=$GITHUB_TOKEN"
