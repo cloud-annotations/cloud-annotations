3141aa8181249bf0f521added229545e0f7b7e30
Mofizur Rahman <moficodes@gmail.com>
2020-01-02 19:31:40 -0500

    added spinner for waiting to cancel

--- cacli/cmd/cancel/cancel.go
+++ cacli/cmd/cancel/cancel.go
@@ -2,9 +2,13 @@ package cancel
 
 import (
 	"errors"
+	"fmt"
+	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/cloud-annotations/training/cacli/cmd/login"
 	"github.com/cloud-annotations/training/cacli/e"
+	"github.com/cloud-annotations/training/cacli/ibmcloud"
 	"github.com/spf13/cobra"
 )
 
@@ -28,13 +32,22 @@ func Run(cmd *cobra.Command, args []string) {
 		e.Exit(errors.New("Can not cancel training. use 'cacli list' to check status"))
 	case "pending", "running":
 		// cancel
-		err = session.CancelRun(modelID)
+		err = loading(modelID, session)
 		if err != nil {
 			e.Exit(errors.New("failed to cancel training"))
 		}
+		fmt.Println("Training canceled")
 	default:
 		// means we gave a bad model id.
 		e.Exit(errors.New("TODO: GetTrainingRun didn't return with a valid state"))
 	}
 
+}
+
+func loading(modelID string, session *ibmcloud.CredentialSession) error {
+	s := spinner.New(spinner.CharSets[14], 60*time.Millisecond)
+	s.Suffix = " Attempting to cancel training... May take a minute or two..."
+	s.Start()
+	defer s.Stop()
+	return session.CancelRun(modelID)
 }
