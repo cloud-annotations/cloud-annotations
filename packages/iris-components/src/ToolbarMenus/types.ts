899d59f49dd121d0a09e588db4f587a9157c844d
Nick Bourdakos <bourdakos1@gmail.com>
2021-05-28 11:19:19 -0400

    Update copyright year in LICENSE

--- packages/iris-components/src/ToolbarMenus/types.ts
+++ packages/iris-components/src/ToolbarMenus/types.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 International Business Machines
+ * Copyright (c) International Business Machines
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
